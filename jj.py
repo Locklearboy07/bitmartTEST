61de54dcdf49ce0466d57cee4c157e6618c29420
Locklearboy07 <141600454+Locklearboy07@users.noreply.github.com>
2023-10-21 07:40:42 -0400

    Update jj.py

--- jj.py
+++ jj.py
@@ -9,37 +9,48 @@ url = "https://api-cloud.bitmart.com/spot/v1/ticker_detail"
 
 api = APISpot(api_key, secret_key, memo, url)
 
-# Function to get the current price of the BNT/USDT trading pair
+# Function to get the current price of the xrp/USDT trading pair
 def get_current_price():
-    ticker = api.get_ticker('BNT_USDT')
+    ticker = api.get_ticker('xrp_USDT')
     return ticker['data']['last_price']
 
-# Function to calculate the moving average of the BNT/USDT pair
+# Function to calculate the moving average of the xrp/USDT pair
 def calculate_moving_average():
-    prices = api.get_candlestick('BNT_USDT', '1h', 21)
+    prices = api.get_candlestick('xrp_USDT', '1h', 21)
     close_prices = [float(price['close']) for price in prices['data']]
     moving_average = talib.SMA(close_prices, timeperiod=20)
     return moving_average[-1]
 
-# Function to place a buy order for BNT at the specified price
-def buy_BNT(price):
-    api.create_order('BNT_USDT', 'limit', 'buy', price, '10')
+# Function to calculate the upper and lower Bollinger Bands of the xrp/USDT pair
+def calculate_bollinger_bands():
+    prices = api.get_candlestick('xrp_USDT', '1h', 21)
+    close_prices = [float(price['close']) for price in prices['data']]
+    upper_band, middle_band, lower_band = talib.BBANDS(close_prices, timeperiod=20)
+    return upper_band[-1], middle_band[-1], lower_band[-1]
 
-# Function to place a sell order for BNT at the specified price
-def sell_BNT(price):
-    api.create_order('BNT_USDT', 'limit', 'sell', price, '10')
+# Function to place a buy order for xrp at the specified price
+def buy_xrp(price):
+    api.create_order('xrp_USDT', 'limit', 'buy', price, '10')
 
-# Main function to monitor the price and moving average and execute buy/sell orders accordingly
+# Function to place a sell order for xrp at the specified price
+def sell_xrp(price):
+    api.create_order('xrp_USDT', 'limit', 'sell', price, '10')
+
+# Main function to monitor the price, moving average, and Bollinger Bands and execute buy/sell orders accordingly
 def main():
     price = get_current_price()
     moving_average = calculate_moving_average()
+    upper_band, middle_band, lower_band = calculate_bollinger_bands()
+
     while True:
         price = get_current_price()
         moving_average = calculate_moving_average()
-        if price > moving_average:
-            buy_BNT(price)
-        elif price < moving_average:
-            sell_BNT(price)
+        upper_band, middle_band, lower_band = calculate_bollinger_bands()
+
+        if price > upper_band:
+            sell_xrp(price)
+        elif price < lower_band:
+            buy_xrp(price)
 
 # Execute the main function if the script is run directly
 if __name__ == "__main__":
