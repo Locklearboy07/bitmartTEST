9004388d6007abb75a493181921f705f1205a847
Locklearboy07 <141600454+Locklearboy07@users.noreply.github.com>
2023-10-23 19:26:18 -0400

    Update jj.py

--- jj.py
+++ jj.py
@@ -7,7 +7,6 @@ api_key = "039c26c7f5316ab9891ed924b3a4885bf35c70fe"
 secret_key = "866cee39cb5a66816c815ee46481530f4d6b984bcf8eb9f9676715ee46fe2e37"
 memo = "5"
 url = "https://api-cloud.bitmart.com/spot/v1/ticker_detail"
-
 api = APISpot(api_key, secret_key, memo, url)
 
 # Function to get the current price of the xrp/USDT trading pair
@@ -24,7 +23,7 @@ def calculate_moving_average():
 
 # Function to calculate the upper and lower Bollinger Bands of the xrp/USDT pair
 def calculate_bollinger_bands():
-    prices = api.get_candlestick('xrp_USDT', '1h', 21)\]
+    prices = api.get_candlestick('xrp_USDT', '1h', 21)
     close_prices = [float(price['close']) for price in prices['data']]
     upper_band, middle_band, lower_band = talib.BBANDS(close_prices, timeperiod=20)
     return upper_band[-1], middle_band[-1], lower_band[-1]
@@ -42,26 +41,21 @@ def main():
     price = get_current_price()
     moving_average = calculate_moving_average()
     upper_band, middle_band, lower_band = calculate_bollinger_bands()
-
     while True:
         price = get_current_price()
         moving_average = calculate_moving_average()
         upper_band, middle_band, lower_band = calculate_bollinger_bands()
-
         if price > upper_band:
             sell_xrp(price)
         elif price < lower_band:
             buy_xrp(price)
 
-# Execute the main function if the script is run directly
-if __name__ == "__main__":
-    main()
-    
 app = Flask(__name__)
 
-     @app.route('/')
-     def hello():
-         return 'Hello, World!'
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
-     if __name__ == '__main__':
-         app.run()
+if __name__ == '__main__':
+    main()
+    app.run()
